a7fa1bba667e1c5a91da23eed29d637f5968b862
Ishiguro Akane <ishiguro-akane@s-giken.com>
2023-08-03 11:29:44 +0900

    氏名検索欄0803

--- src/main/java/com/s_giken/training/webapp/repository/MemberRepository.java
+++ src/main/java/com/s_giken/training/webapp/repository/MemberRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.s_giken.training.webapp.model.Member;
+//import org.springframework.data.jpa.repository.Query;
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
     // SprintBootでは、予め用意されているのリポジトリアクセス用のメソッド以外のメソッドを
