7c1c55df82372691982c91733e0c8b3ce36f1b02
Ishiguro Akane <ishiguro-akane@s-giken.com>
2023-08-07 11:22:31 +0900

    料金検索画面0807

---
+++ src/main/java/com/s_giken/training/webapp/model/Charge.java
@@ -0,0 +1,60 @@
+package com.s_giken.training.webapp.model;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "T_CHARGE") // 対応するデータベーステーブル名
+@Data // メンバー変数に対するゲッター・セッターを自動生成
+@NoArgsConstructor // 引数のないコンストラクタを自動生成
+@AllArgsConstructor // 全てのメンバ変数に対する引数を持つコンストラクタを自動生成
+public class Charge {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "charge_id") // 料金ID
+    private int chargeId;
+
+    @Column(name = "name") // 料金名
+    @NotNull
+    @NotBlank
+    private String name;
+
+    @Column(name = "char_method") // 種別
+    @NotNull
+    private int charMethod;
+
+    @Column(name = "charge") // 料金
+    @NotNull
+    private String charge;
+
+    @Column(name = "start_date") // 適用開始日
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @NotNull
+    private Date startDate;
+
+    @Column(name = "end_date") // 適用終了日
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date endDate;
+
+    @CreatedDate
+    private Timestamp createdDate;
+
+    @LastModifiedDate
+    private Timestamp updatedDate;
+}
