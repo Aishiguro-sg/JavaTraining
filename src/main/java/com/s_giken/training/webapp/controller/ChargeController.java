c1da650a3b9aedb1f8b6108876e77f8d09b2c6d4
Ishiguro Akane <ishiguro-akane@s-giken.com>
2023-08-09 11:48:08 +0900

    エラー0809

--- src/main/java/com/s_giken/training/webapp/controller/ChargeController.java
+++ src/main/java/com/s_giken/training/webapp/controller/ChargeController.java
@@ -65,11 +65,12 @@ public class ChargeController {
     public String saveCharge(
             @Validated Charge charge,
             BindingResult bindingResult,
+            // 保存通知
+            // エラーが出た時
             RedirectAttributes redirectAttributes) {
         if (bindingResult.hasErrors()) {
-            return "charge_edit";
+            return "/charge_edit";
         }
-        // 保存通知
         chargeService.save(charge);
         redirectAttributes.addFlashAttribute("message", "保存しました。");
         return "redirect:/charge/edit/" + charge.getChargeId();
@@ -78,8 +79,9 @@ public class ChargeController {
     @GetMapping("/delete/{id}")
     public String deleteCharge(
             @PathVariable int id,
+            // 削除通知
             RedirectAttributes redirectAttributes) {
-        // 削除通知
+
         chargeService.deleteById(id);
         redirectAttributes.addFlashAttribute("message", "削除しました。");
         return "redirect:/charge/search";
